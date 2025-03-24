c685c2e1f52cf3ed722667a3df7af7eaddf09cdb
jmgirard <me@jmgirard.com>
2025-03-23 21:14:07 -0500

    update base image

--- bayes/mount.sh
+++ bayes/mount.sh
@@ -12,9 +12,9 @@ mkdir -p /mnt/datasets /mnt/datasets
 
 # mount datasets server using temporary credentials file
 mount -t cifs -o credentials=/home/rstudio/.smbcredentials,uid=1000,gid=1000,file_mode=0777,dir_mode=0777 //resfs.home.ku.edu/groups_hipaa/lsi/jgirard/general/datasets /mnt/datasets
-    
+
 # mount projects server using temporary credentials file
 mount -t cifs -o credentials=/home/rstudio/.smbcredentials,uid=1000,gid=1000,file_mode=0777,dir_mode=0777 //resfs.home.ku.edu/groups_hipaa/lsi/jgirard/general/projects /mnt/projects
-    
+
 # securely delete temporary credentials file
 shred -u /home/rstudio/.smbcredentials
